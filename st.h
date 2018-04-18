202a129dc46a256a75b6eca9d39cd1937f212931
Nicolás Salas V <nikosalas@gmail.com>
2018-04-18 19:15:46 -0300

    8.0-arch: Apply shift+mousewheel patch

--- st.h
+++ st.h
@@ -81,6 +81,13 @@ typedef union {
 	const void *v;
 } Arg;
 
+typedef struct {
+	uint b;
+	uint mask;
+	void (*func)(const Arg *);
+	const Arg arg;
+} MouseKey;
+
 void die(const char *, ...);
 void redraw(void);
 void draw(void);
@@ -130,3 +137,4 @@ extern char *termname;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
+extern MouseKey mkeys[];
